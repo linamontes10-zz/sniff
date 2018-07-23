27acbfd6ad13f66a7a2035761204264b4a848df2
Demo User <demouser@MacBook-Pro-6.local>
2018-07-23 16:18:53 -0400

    wibviw

--- static/scripts/sniff.js
+++ static/scripts/sniff.js
@@ -1,7 +1,8 @@
 function myMap() {
   var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
+      center:new google.maps.LatLng(40.7128,-74.0060),
+      zoom:10,
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  
 }
