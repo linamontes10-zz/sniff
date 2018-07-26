1554eb29fe3da3919df89488a52085e82937ead6
Google NYC <demouser@MacBook-Pro-3.local>
2018-07-26 10:32:58 -0400

    k

--- static/scripts/dogparks.js
+++ static/scripts/dogparks.js
@@ -1,15 +1,15 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById('navbar');
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// var sticky = navbar.offsetTop;
+//
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
  function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
